89c37dbe0a00029d477451ff68588ec58e055593
jecooooo <94885341+iamvergel@users.noreply.github.com>
2025-04-02 19:43:15 +0800

    sasa

--- admin/config/dbconn.php
+++ admin/config/dbconn.php
@@ -1,8 +1,13 @@
 <?php
 
-$sname = "localhost";
-$uname = "root";
-$password = "";
+// $sname = "localhost";
+// $uname = "root";
+// $password = "";
+// $db_name = "tala";
+
+$sname = "database-1.ctaowmq2kfea.ap-southeast-2.rds.amazonaws.com";
+$uname = "admin";
+$password = "CIH0qrwpNwceRdQ6cY2i";
 $db_name = "tala";
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
